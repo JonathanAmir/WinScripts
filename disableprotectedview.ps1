36bf83886f27c14be8748289c8e4372031043923
JonathanAmir <106614153+JonathanAmir@users.noreply.github.com>
2023-11-16 15:12:03 +0200

    Add files via upload

---
+++ disableprotectedview.ps1
@@ -0,0 +1,24 @@
+﻿#Created by Jonathan Amir for Impact Netowrks ldt.
+
+function disableProtectView {
+
+$applications = 'word','excel'
+$registeryNames = 'DisableAttachmentsInPV','DisableInternetFilesInPV','DisableUnsafeLocationsInPV'
+
+$regPath = "HKCU:\SOFTWARE\Microsoft\Office\16.0\$application\Security\ProtectedView"
+foreach ($application in $applications) {
+    if  (Test-Path -Path $regPath) {
+        Write-output "Information" "No need for further action"
+                    exit
+        
+}
+    else {
+        foreach ($regName in $registeryNames) {
+        New-Item -Path HKCU:\SOFTWARE\Microsoft\Office\16.0\$application\Security\ProtectedView 
+        New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Office\16.0\$application\Security\ProtectedView -name $regName -Value 1 -PropertyType DWORD -Force 
+        Write-Output "Information: Disabling Protected View...."
+        }
+}
+}
+}
+disableProtectView
