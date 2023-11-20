5f4700062f09b2f2ca88a43233580756744325fb
JonathanAmir <106614153+JonathanAmir@users.noreply.github.com>
2023-11-20 09:14:11 +0200

    Update disableprotectedview.ps1

--- disableprotectedview.ps1
+++ disableprotectedview.ps1
@@ -7,18 +7,19 @@ function disableProtectView {
 $applications = 'word','excel'
 $registeryNames = 'DisableAttachmentsInPV','DisableInternetFilesInPV','DisableUnsafeLocationsInPV'
 
-$regPath = "HKCU:\SOFTWARE\Microsoft\Office\16.0\$application\Security\ProtectedView"
 foreach ($application in $applications) {
+$regPath = "HKCU:\SOFTWARE\Microsoft\Office\16.0\$application\Security\ProtectedView"
+
     if  (Test-Path -Path $regPath) {
         Write-output "Information" "No need for further action"
                     exit
         
 }
     else {
+        New-Item -Path $regPath -Force
         foreach ($regName in $registeryNames) {
-        New-Item -Path HKCU:\SOFTWARE\Microsoft\Office\16.0\$application\Security\ProtectedView 
-        New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Office\16.0\$application\Security\ProtectedView -name $regName -Value 1 -PropertyType DWORD -Force 
-        Write-Output "Information: Disabling Protected View...."
+            New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Office\16.0\$application\Security\ProtectedView -name $regName -Value 1 -PropertyType DWORD -Force 
+        Write-Output "Information: Disabling Protected View for $application...."
         }
 }
 }
